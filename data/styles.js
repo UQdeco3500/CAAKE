069d5162ba4479b4a1c8ed693f15ea37d002122c
Abi-B-28 <abigailborromeo@outlook.com>
2023-10-21 15:38:46 +1000

    Update styles.js

--- data/styles.js
+++ data/styles.js
@@ -3,3 +3,19 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
     // put styles in here
 }) 
+
+export const colours = {
+    blue = "#4ABCDE",
+    darkBlue = "#126F90",
+    gray = "#5F6B6B",
+    lightGray = "#897676",
+};
+
+export const sizes = {
+    // title: 
+
+};
+
+export const fonts = {
+    
+};
