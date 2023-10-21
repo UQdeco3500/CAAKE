55291b11727b53dbb74b219fe072e51a1a1c330f
Abi-B-28 <abigailborromeo@outlook.com>
2023-10-21 17:22:46 +1000

    Update Tabs.js

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,17 +1,41 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from '@react-navigation/native';
 import PictureScreen from "../screens/PictureScreen";
 import HomeScreen from "../screens/HomeScreen";
 import PhoneCamera from "./PhoneCamera";
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => {
+const TabIcon =(icon, color) => {
+    return(
+        <FontAwesome5 name={icon} size={28} color={color ? color : '#000'} />
+    )
+}
+const Tabs =() => {
     return (
         <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'HomeScreen') {
+                        iconName = focused ? 'paper-plane' : 'paper-plane';
+                    }
+                    else if (route.name === 'PhoneCamera'){
+                        iconName = focused ? 'images' : 'images';
+                    }
+                    return <FontAwesome5 name={iconName} size={size} color={color} />
+                },
+            })}
+
+                tabBarOptions={{
+                    activeTintColor: "#126F90",
+                    inactiveTintColor: 'gray',
+            }}
 
         >
-
             <Tab.Screen 
                 name="HomeScreen"
                 component={HomeScreen}
@@ -28,7 +52,7 @@ const Tabs = () => {
                 }}
             />
         </Tab.Navigator>
-    )
+    );
 }
 
 export default Tabs;
